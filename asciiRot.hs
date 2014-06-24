16ae8b148c92f35aacac4fe4aa85735cd6a79eb3
42f87d89 <gbuschle@hotmail.com>
2014-06-24 13:01:58 +0000

    More advanced rotation

--- asciiRot.hs
+++ asciiRot.hs
@@ -1,4 +1,17 @@
+import System.Environment
+import System.IO
+
+asciiRotC :: Int -> Char -> Char
+asciiRotC 0 msg = msg
+asciiRotC n msg = toEnum (33 + (n + (fromEnum msg) - 33) `mod` 93) :: Char
+
 asciiRot :: Int -> String -> String
-asciiRot _ "" = ""
-asciiRot 0 msg = msg
-asciiRot n msg = map (\x -> toEnum (33 + (n + (fromEnum x) - 33) `mod` 93) :: Char) msg
+asciiRot n msg = map (asciiRotC n) msg
+
+asciiRotWith :: (Int->Int) -> Int -> String -> String
+asciiRotWith _ _ "" = ""
+asciiRotWith f acc (x:xs) = (asciiRotC acc x):(asciiRotWith f (f acc) xs)
+
+main = do
+    args <- getArgs
+    putStrLn $ asciiRotWith (+1) 0 (head args)
